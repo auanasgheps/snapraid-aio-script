c8dc7e381a9a2bc4ee393824f8a5953ce361469f
Oliver Cervera <cervera93-19@yahoo.it>
2021-01-08 18:33:42 +0100

    Update snapraid-aio-script.sh
    Move user variables to another file

--- snapraid-aio-script.sh
+++ snapraid-aio-script.sh
@@ -29,7 +29,7 @@
 #   Original by Zack Reed https://zackreed.me/snapraid-split-parity-sync-script/
 #   + mtompkins https://gist.github.com/mtompkins/91cf0b8be36064c237da3f39ff5cc49d
 #   
-SNAPSCRIPTVERSION="2.6.6"
+SNAPSCRIPTVERSION="2.7.0.DEV-1"
 
 ########################################################################
 
@@ -37,90 +37,25 @@ SNAPSCRIPTVERSION="2.6.6"
 #   USER VARIABLES   #
 ######################
 
-####################### USER CONFIGURATION START #######################
+# declare the current path
+CURRENT_DIR="$(dirname "${0}")"
 
-# address where the output of the jobs will be emailed to.
-EMAIL_ADDRESS="youremailgoeshere"
+# declare config file variable for this script 
+source $CURRENT_DIR/config.sh
 
-# Set the threshold of deleted files to stop the sync job from running.
-# NOTE that depending on how active your filesystem is being used, a low
-# number here may result in your parity info being out of sync often and/or
-# you having to do lots of manual syncing.
-DEL_THRESHOLD=500
-UP_THRESHOLD=500
+########################################################################
 
-# Set number of warnings before we force a sync job.
-# This option comes in handy when you cannot be bothered to manually
-# start a sync job when DEL_THRESHOLD is breached due to false alarm.
-# Set to 0 to ALWAYS force a sync (i.e. ignore the delete threshold above)
-# Set to -1 to NEVER force a sync (i.e. need to manual sync if delete threshold is breached)
-SYNC_WARN_THRESHOLD=-1
-
-# Set percentage of array to scrub if it is in sync.
-# i.e. 0 to disable and 100 to scrub the full array in one go
-# WARNING - depending on size of your array, setting to 100 will take a very long time!
-SCRUB_PERCENT=5
-SCRUB_AGE=10
-
-# Prehash Data To avoid the risk of a latent hardware issue, you can enable the "pre-hash" mode and have all the
-# data read two times to ensure its integrity. This option also verifies the files moved inside the array, to ensure
-# that the move operation went successfully, and in case to block the sync and to allow to run a fix operation.
-# 1 to enable, any other values to disable
-PREHASH=1
-
-# Set the option to log SMART info. 1 to enable, any other value to disable
-SMART_LOG=1
-
-# Set verbosity of the email output. TOUCH and DIFF outputs will be kept in the email, producing a potentially huge email. Keep this disabled for optimal reading
-# You can always check TOUCH and DIFF outputs using the TMP file.
-# 1 to enable, any other values to disable
-VERBOSITY=0
-
-# Set if disk spindown should be performed. Depending on your system, this may not work. 1 to enable, any other values to disable
-SPINDOWN=0
-
-# location of the snapraid binary
-SNAPRAID_BIN="/usr/bin/snapraid"
-# location of the mail program binary
-MAIL_BIN="/usr/bin/mailx"
+######################
+#   MAIN SCRIPT   #
+######################
 
 function main(){
-
-  ######################
-  #   INIT VARIABLES   #
-  ######################
-  CHK_FAIL=0
-  DO_SYNC=0
-  EMAIL_SUBJECT_PREFIX="(SnapRAID on `hostname`)"
-  GRACEFUL=0
-  SYNC_WARN_FILE="/tmp/snapRAID.warnCount"
-  SYNC_WARN_COUNT=""
-  TMP_OUTPUT="/tmp/snapRAID.out"
-  SNAPRAID_LOG="/var/log/snapraid.log"
-  # Capture time
-  SECONDS=0
-
-  # Build Services Array...
-  service_array_setup
-
-  # Expand PATH for smartctl
-  PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-
-  # Determine names of first content file...
-  CONTENT_FILE=`grep -v '^$\|^\s*\#' /etc/snapraid.conf | grep snapraid.content | head -n 1 | cut -d " " -f2`
-
-  # Build an array of parity all files...
-  PARITY_FILES[0]=`grep -v '^$\|^\s*\#' /etc/snapraid.conf | grep snapraid.parity | head -n 1 | cut -d " " -f2`
-  IFS=$'\n' PARITY_FILES=(`cat /etc/snapraid.conf | grep "^[^#;]" | grep "^\([2-6z]-\)*parity" | cut -d " " -f 2 | tr ',' '\n'`)
-
-##### USER CONFIGURATION STOP ##### MAKE NO CHANGES BELOW THIS LINE ####
-
   # create tmp file for output
   > $TMP_OUTPUT
 
   # Redirect all output to file and screen. Starts a tee process
   output_to_file_screen
-
+ 
   # timestamp the job
   echo "SnapRAID Script Job started [`date`]"
   echo "Running SnapRAID version $SNAPRAIDVERSION"
@@ -133,9 +68,19 @@ function main(){
   mklog "INFO: SnapRAID Script version $SNAPSCRIPTVERSION"
 
 
-  # Remove any plex created anomalies
   echo "##Preprocessing"
 
+ # Check if script configuration file has been found
+  if [ ! -f $CURRENT_DIR/config.sh ]; 
+  then
+    echo "Script configuration file not found! The script cannot be run! Please check and try again!"
+	mklog "WARN: Script configuration file not found! The script cannot be run! Please check and try again!"
+	exit 1;
+  else
+	echo "Configuration file found! Proceeding."
+	mklog "INFO: Script configuration file found! Proceeding."
+  fi
+  
   # sanity check first to make sure we can access the content and parity files
   mklog "INFO: Checking SnapRAID disks"
   sanity_check
