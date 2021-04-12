dc2b1b58fb9b36c392c739f7991c0a5287982b71
Casey McGinty <casey.mcginty@gmail.com>
2021-04-12 00:57:06 -0700

    Fix indentation

--- snapraid-aio-script.sh
+++ snapraid-aio-script.sh
@@ -45,26 +45,24 @@ function main(){
 
   echo "##Preprocessing"
 
- # Check if script configuration file has been found
-  if [ ! -f "$CONFIG_FILE" ];
-  then
+  # Check if script configuration file has been found
+  if [ ! -f "$CONFIG_FILE" ]; then
     echo "Script configuration file not found! The script cannot be run! Please check and try again!"
-	mklog "WARN: Script configuration file not found! The script cannot be run! Please check and try again!"
-	exit 1;
+    mklog "WARN: Script configuration file not found! The script cannot be run! Please check and try again!"
+    exit 1;
   else
-	echo "Configuration file found! Proceeding."
-	mklog "INFO: Script configuration file found! Proceeding."
+    echo "Configuration file found! Proceeding."
+    mklog "INFO: Script configuration file found! Proceeding."
   fi
 
   # install markdown if not present
-  if [ "$(dpkg-query -W -f='${Status}' python-markdown 2>/dev/null | grep -c "ok installed")" -eq 0 ];
-  then
-	echo "**Markdown has not been found and will be installed.**"
-	mklog "WARN: Markdown has not been found and will be installed."
-	# super silent and secret install command
-	export DEBIAN_FRONTEND=noninteractive
-	apt-get install -qq -o=Dpkg::Use-Pty=0 python-markdown;
-	echo
+  if [ "$(dpkg-query -W -f='${Status}' python-markdown 2>/dev/null | grep -c "ok installed")" -eq 0 ]; then
+    echo "**Markdown has not been found and will be installed.**"
+    mklog "WARN: Markdown has not been found and will be installed."
+    # super silent and secret install command
+    export DEBIAN_FRONTEND=noninteractive
+    apt-get install -qq -o=Dpkg::Use-Pty=0 python-markdown;
+    echo
   fi
 
   # sanity check first to make sure we can access the content and parity files
@@ -124,7 +122,7 @@ function main(){
   else
     # NO, so let's skip SYNC
     echo "No change detected. Not running SYNC job. [$(date)]"
-  mklog "INFO: No change detected. Not running SYNC job."
+    mklog "INFO: No change detected. Not running SYNC job."
     DO_SYNC=0
   fi
 
@@ -132,9 +130,9 @@ function main(){
   if [ "$DO_SYNC" -eq 1 ]; then
     echo "SYNC is authorized. [$(date)]"
     echo "###SnapRAID SYNC [$(date)]"
-  mklog "INFO: SnapRAID SYNC Job started"
-  if [ "$PREHASH" -eq 1 ]; then
-    $SNAPRAID_BIN sync -h -q
+    mklog "INFO: SnapRAID SYNC Job started"
+    if [ "$PREHASH" -eq 1 ]; then
+      $SNAPRAID_BIN sync -h -q
     else
       $SNAPRAID_BIN sync -q
     fi
@@ -142,7 +140,7 @@ function main(){
     close_output_and_wait
     output_to_file_screen
     echo "SYNC finished [$(date)]"
-  mklog "INFO: SnapRAID SYNC Job finished"
+    mklog "INFO: SnapRAID SYNC Job finished"
     JOBS_DONE="$JOBS_DONE + SYNC"
     # insert SYNC marker to 'Everything OK' or 'Nothing to do' string to differentiate it from SCRUB job later
     sed_me "s/^Everything OK/**SYNC JOB - Everything OK**/g;s/^Nothing to do/**SYNC JOB - Nothing to do**/g" "$TMP_OUTPUT"
@@ -159,26 +157,26 @@ function main(){
     # YES, first let's check if delete threshold has been breached and we have not forced a sync.
     if [ "$CHK_FAIL" -eq 1 ] && [ "$DO_SYNC" -eq 0 ]; then
       # YES, parity is out of sync so let's not run scrub job
-	  echo
+      echo
       echo "Scrub job is cancelled as parity info is out of sync (deleted or changed files threshold has been breached). [$(date)]"
-    mklog "INFO: Scrub job is cancelled as parity info is out of sync (deleted or changed files threshold has been breached)."
+      mklog "INFO: Scrub job is cancelled as parity info is out of sync (deleted or changed files threshold has been breached)."
     else
       # NO, delete threshold has not been breached OR we forced a sync, but we have one last test -
       # let's make sure if sync ran, it completed successfully (by checking for our marker text "SYNC JOB -" in the output).
       if [ "$DO_SYNC" -eq 1 ] && ! grep -qw "SYNC JOB -" "$TMP_OUTPUT"; then
         # Sync ran but did not complete successfully so lets not run scrub to be safe
-          echo "**WARNING** - check output of SYNC job. Could not detect marker. Not proceeding with SCRUB job. [$(date)]"
-    mklog "WARN: Check output of SYNC job. Could not detect marker. Not proceeding with SCRUB job."
+        echo "**WARNING** - check output of SYNC job. Could not detect marker. Not proceeding with SCRUB job. [$(date)]"
+        mklog "WARN: Check output of SYNC job. Could not detect marker. Not proceeding with SCRUB job."
       else
         # Everything ok - let's run the scrub job!
         echo "###SnapRAID SCRUB [$(date)]"
-    mklog "INFO: SnapRAID SCRUB Job started"
+        mklog "INFO: SnapRAID SCRUB Job started"
         $SNAPRAID_BIN scrub -p $SCRUB_PERCENT -o $SCRUB_AGE -q
         #wait for the job to finish
         close_output_and_wait
         output_to_file_screen
         echo "SCRUB finished [$(date)]"
-    mklog "INFO: SnapRAID SCRUB Job finished"
+        mklog "INFO: SnapRAID SCRUB Job finished"
         echo
         JOBS_DONE="$JOBS_DONE + SCRUB"
         # insert SCRUB marker to 'Everything OK' or 'Nothing to do' string to differentiate it from SYNC job above
@@ -187,7 +185,7 @@ function main(){
     fi
   else
     echo "Scrub job is not enabled. Not running SCRUB job. [$(date)]"
-  mklog "INFO: Scrub job is not enabled. Not running SCRUB job."
+    mklog "INFO: Scrub job is not enabled. Not running SCRUB job."
   fi
 
   echo
@@ -200,7 +198,7 @@ function main(){
     $SNAPRAID_BIN smart
     close_output_and_wait
     output_to_file_screen
-	echo
+    echo
   fi
 
   # Show SnapRAID Status information if enabled
@@ -210,13 +208,13 @@ function main(){
     $SNAPRAID_BIN status
     close_output_and_wait
     output_to_file_screen
-	echo
+    echo
   fi
 
 
   # Spinning down disks (Method 1: snapraid - preferred)
   if [ "$SPINDOWN" -eq 1 ]; then
-  $SNAPRAID_BIN down
+    $SNAPRAID_BIN down
   fi
 
   # Spinning down disks (Method 2: hdparm - spins down all rotational devices)
@@ -253,15 +251,15 @@ function main(){
     echo
     echo "----------------------------------------"
     echo "##Total time elapsed for SnapRAID: $ELAPSED"
-	mklog "INFO: Total time elapsed for SnapRAID: $ELAPSED"
+    mklog "INFO: Total time elapsed for SnapRAID: $ELAPSED"
 
     # Add a topline to email body
     sed_me "1s:^:##$SUBJECT \n:" "${TMP_OUTPUT}"
-  if [ $VERBOSITY -eq 1 ]; then
-   send_mail_verbose
-  else
-   send_mail
-  fi
+    if [ $VERBOSITY -eq 1 ]; then
+      send_mail_verbose
+    else
+      send_mail
+    fi
   fi
 
   exit 0;
@@ -274,9 +272,9 @@ function main(){
 function sanity_check() {
   if [ ! -e "$CONTENT_FILE" ]; then
     echo "**ERROR** Content file ($CONTENT_FILE) not found!"
-  echo "**ERROR**: Please check the status of your disks! The script exits here due to missing file or disk..."
-  mklog "WARN: Content file ($CONTENT_FILE) not found!"
-  mklog "WARN: Please check the status of your disks! The script exits here due to missing file or disk..."
+    echo "**ERROR**: Please check the status of your disks! The script exits here due to missing file or disk..."
+    mklog "WARN: Content file ($CONTENT_FILE) not found!"
+    mklog "WARN: Please check the status of your disks! The script exits here due to missing file or disk..."
     prepare_mail
     # Add a topline to email body
     sed_me "1s:^:##$SUBJECT \n:" "${TMP_OUTPUT}"
@@ -286,20 +284,19 @@ function sanity_check() {
 
   echo "Testing that all parity files are present."
   mklog "INFO: Testing that all parity files are present."
-  for i in "${PARITY_FILES[@]}"
-    do
-      if [ ! -e "$i" ]; then
-        echo "[$(date)] ERROR - Parity file ($i) not found!"
-        echo "ERROR - Parity file ($i) not found!" >> "$TMP_OUTPUT"
-    echo "**ERROR**: Please check the status of your disks! The script exits here due to missing file or disk..."
-    mklog "WARN: Parity file ($i) not found!"
-    mklog "WARN: Please check the status of your disks! The script exits here due to missing file or disk..."
+  for i in "${PARITY_FILES[@]}"; do
+    if [ ! -e "$i" ]; then
+      echo "[$(date)] ERROR - Parity file ($i) not found!"
+      echo "ERROR - Parity file ($i) not found!" >> "$TMP_OUTPUT"
+      echo "**ERROR**: Please check the status of your disks! The script exits here due to missing file or disk..."
+      mklog "WARN: Parity file ($i) not found!"
+      mklog "WARN: Please check the status of your disks! The script exits here due to missing file or disk..."
       prepare_mail
       # Add a topline to email body
       sed_me "1s:^:##$SUBJECT \n:" "${TMP_OUTPUT}"
       send_mail
-        exit;
-      fi
+      exit;
+    fi
   done
   echo "All parity files found. Continuing..."
 }
@@ -324,77 +321,79 @@ function sed_me(){
 
 function chk_del(){
   if [ "$DEL_COUNT" -lt "$DEL_THRESHOLD" ]; then
-	if [ "$DEL_COUNT" -eq 0 ]; then
-	echo "There are no deleted files, that's fine."
-	DO_SYNC=1
-	else
-    echo "There are deleted files. The number of deleted files ($DEL_COUNT) is below the threshold of ($DEL_THRESHOLD)."
-    DO_SYNC=1
-	fi
+    if [ "$DEL_COUNT" -eq 0 ]; then
+      echo "There are no deleted files, that's fine."
+      DO_SYNC=1
+    else
+      echo "There are deleted files. The number of deleted files ($DEL_COUNT) is below the threshold of ($DEL_THRESHOLD)."
+      DO_SYNC=1
+    fi
   else
     echo "**WARNING** Deleted files ($DEL_COUNT) reached/exceeded threshold ($DEL_THRESHOLD)."
-  mklog "WARN: Deleted files ($DEL_COUNT) reached/exceeded threshold ($DEL_THRESHOLD)."
+    mklog "WARN: Deleted files ($DEL_COUNT) reached/exceeded threshold ($DEL_THRESHOLD)."
     CHK_FAIL=1
   fi
 }
 
 function chk_updated(){
   if [ "$UPDATE_COUNT" -lt "$UP_THRESHOLD" ]; then
-	if [ "$UPDATE_COUNT" -eq 0 ]; then
-	echo "There are no updated files, that's fine."
-	DO_SYNC=1
-	else
-    echo "There are updated files. The number of updated files ($UPDATE_COUNT) is below the threshold of ($UP_THRESHOLD)."
-    DO_SYNC=1
-	fi
+    if [ "$UPDATE_COUNT" -eq 0 ]; then
+      echo "There are no updated files, that's fine."
+      DO_SYNC=1
+    else
+      echo "There are updated files. The number of updated files ($UPDATE_COUNT) is below the threshold of ($UP_THRESHOLD)."
+      DO_SYNC=1
+    fi
   else
     echo "**WARNING** Updated files ($UPDATE_COUNT) reached/exceeded threshold ($UP_THRESHOLD)."
-  mklog "WARN: Updated files ($UPDATE_COUNT) reached/exceeded threshold ($UP_THRESHOLD)."
+    mklog "WARN: Updated files ($UPDATE_COUNT) reached/exceeded threshold ($UP_THRESHOLD)."
     CHK_FAIL=1
   fi
 }
 
 function chk_sync_warn(){
   if [ "$SYNC_WARN_THRESHOLD" -gt -1 ]; then
-	if [ "$SYNC_WARN_THRESHOLD" -eq 0 ]; then
-	echo "Forced sync is enabled."
-  mklog "INFO: Forced sync is enabled."
-	else
-	echo "Sync after threshold warning(s) is enabled."
-  mklog "INFO: Sync after threshold warning(s) is enabled."
-  	fi
+    if [ "$SYNC_WARN_THRESHOLD" -eq 0 ]; then
+      echo "Forced sync is enabled."
+      mklog "INFO: Forced sync is enabled."
+    else
+      echo "Sync after threshold warning(s) is enabled."
+      mklog "INFO: Sync after threshold warning(s) is enabled."
+    fi
+
     SYNC_WARN_COUNT=$(sed 'q;/^[0-9][0-9]*$/!d' "$SYNC_WARN_FILE" 2>/dev/null)
     SYNC_WARN_COUNT=${SYNC_WARN_COUNT:-0} #value is zero if file does not exist or does not contain what we are expecting
-	if [ "$SYNC_WARN_COUNT" -ge "$SYNC_WARN_THRESHOLD" ]; then
-	  # force a sync
-	  # if the warn count is zero it means the sync was already forced, do not output a dumb message and continue with the sync job.
+
+    if [ "$SYNC_WARN_COUNT" -ge "$SYNC_WARN_THRESHOLD" ]; then
+      # force a sync
+      # if the warn count is zero it means the sync was already forced, do not output a dumb message and continue with the sync job.
       if [ "$SYNC_WARN_COUNT" -eq 0 ]; then
-	  echo
-	  DO_SYNC=1
-	  else
-      # if there is at least one warn count, output a message and force a sync job. Do not need to remove warning marker here as it is automatically removed when the sync job is run by this script
-      echo "Number of threshold warning(s) ($SYNC_WARN_COUNT) has reached/exceeded threshold ($SYNC_WARN_THRESHOLD). Forcing a SYNC job to run."
-    mklog "INFO: Number of threshold warning(s) ($SYNC_WARN_COUNT) has reached/exceeded threshold ($SYNC_WARN_THRESHOLD). Forcing a SYNC job to run."
-      DO_SYNC=1
-	  fi
+        echo
+        DO_SYNC=1
+      else
+        # if there is at least one warn count, output a message and force a sync job. Do not need to remove warning marker here as it is automatically removed when the sync job is run by this script
+        echo "Number of threshold warning(s) ($SYNC_WARN_COUNT) has reached/exceeded threshold ($SYNC_WARN_THRESHOLD). Forcing a SYNC job to run."
+        mklog "INFO: Number of threshold warning(s) ($SYNC_WARN_COUNT) has reached/exceeded threshold ($SYNC_WARN_THRESHOLD). Forcing a SYNC job to run."
+        DO_SYNC=1
+      fi
     else
       # NO, so let's increment the warning count and skip the sync job
       ((SYNC_WARN_COUNT += 1))
       echo "$SYNC_WARN_COUNT" > "$SYNC_WARN_FILE"
-	  if [ "$SYNC_WARN_COUNT" == "$SYNC_WARN_THRESHOLD" ]; then
-		echo  "This is the **last** warning left. **NOT** proceeding with SYNC job. [$(date)]"
-		mklog "This is the **last** warning left. **NOT** proceeding with SYNC job. [$(date)]"
-		DO_SYNC=0
-	  else
-		echo "$((SYNC_WARN_THRESHOLD - SYNC_WARN_COUNT)) threshold warning(s) until the next forced sync. **NOT** proceeding with SYNC job. [$(date)]"
-		mklog "INFO: $((SYNC_WARN_THRESHOLD - SYNC_WARN_COUNT)) threshold warning(s) until the next forced sync. **NOT** proceeding with SYNC job."
-		DO_SYNC=0
+      if [ "$SYNC_WARN_COUNT" == "$SYNC_WARN_THRESHOLD" ]; then
+        echo  "This is the **last** warning left. **NOT** proceeding with SYNC job. [$(date)]"
+        mklog "This is the **last** warning left. **NOT** proceeding with SYNC job. [$(date)]"
+        DO_SYNC=0
+      else
+        echo "$((SYNC_WARN_THRESHOLD - SYNC_WARN_COUNT)) threshold warning(s) until the next forced sync. **NOT** proceeding with SYNC job. [$(date)]"
+        mklog "INFO: $((SYNC_WARN_THRESHOLD - SYNC_WARN_COUNT)) threshold warning(s) until the next forced sync. **NOT** proceeding with SYNC job."
+        DO_SYNC=0
+      fi
     fi
-	fi
   else
     # NO, so let's skip SYNC
     echo "Forced sync is not enabled. Check $TMP_OUTPUT for details. **NOT** proceeding with SYNC job. [$(date)]"
-  mklog "INFO: Forced sync is not enabled. Check $TMP_OUTPUT for details. **NOT** proceeding with SYNC job."
+    mklog "INFO: Forced sync is not enabled. Check $TMP_OUTPUT for details. **NOT** proceeding with SYNC job."
     DO_SYNC=0
   fi
 }
@@ -412,7 +411,7 @@ function chk_zero(){
     echo "TOUCH finished [$(date)]"
   else
     echo "No zero sub-second timestamp files found."
-  echo "TOUCH finished [$(date)]"
+    echo "TOUCH finished [$(date)]"
   fi
 }
 
@@ -426,20 +425,20 @@ function prepare_mail() {
       MSG="Forced sync with deleted files ($DEL_COUNT) / ($DEL_THRESHOLD) violation"
     fi
 
-  if [ "$UPDATE_COUNT" -ge "$UP_THRESHOLD" ] && [ "$DO_SYNC" -eq 0 ]; then
+    if [ "$UPDATE_COUNT" -ge "$UP_THRESHOLD" ] && [ "$DO_SYNC" -eq 0 ]; then
       MSG="Changed files ($UPDATE_COUNT) / ($UP_THRESHOLD) violation"
     fi
 
-  if [ "$UPDATE_COUNT" -ge "$UP_THRESHOLD" ] && [ "$DO_SYNC" -eq 1 ]; then
+    if [ "$UPDATE_COUNT" -ge "$UP_THRESHOLD" ] && [ "$DO_SYNC" -eq 1 ]; then
       MSG="Forced sync with changed files ($UPDATE_COUNT) / ($UP_THRESHOLD) violation"
     fi
 
-  if [ "$DEL_COUNT" -ge  "$DEL_THRESHOLD" ] && [ "$UPDATE_COUNT" -ge "$UP_THRESHOLD" ] && [ "$DO_SYNC" -eq 0 ]; then
-     MSG="Multiple violations - Deleted files ($DEL_COUNT) / ($DEL_THRESHOLD) and changed files ($UPDATE_COUNT) / ($UP_THRESHOLD)"
+    if [ "$DEL_COUNT" -ge  "$DEL_THRESHOLD" ] && [ "$UPDATE_COUNT" -ge "$UP_THRESHOLD" ] && [ "$DO_SYNC" -eq 0 ]; then
+      MSG="Multiple violations - Deleted files ($DEL_COUNT) / ($DEL_THRESHOLD) and changed files ($UPDATE_COUNT) / ($UP_THRESHOLD)"
     fi
 
-  if [ "$DEL_COUNT" -ge  "$DEL_THRESHOLD" ] && [ "$UPDATE_COUNT" -ge "$UP_THRESHOLD" ] && [ "$DO_SYNC" -eq 1 ]; then
-     MSG="Sync forced with multiple violations - Deleted files ($DEL_COUNT) / ($DEL_THRESHOLD) and changed files ($UPDATE_COUNT) / ($UP_THRESHOLD)"
+    if [ "$DEL_COUNT" -ge  "$DEL_THRESHOLD" ] && [ "$UPDATE_COUNT" -ge "$UP_THRESHOLD" ] && [ "$DO_SYNC" -eq 1 ]; then
+      MSG="Sync forced with multiple violations - Deleted files ($DEL_COUNT) / ($DEL_THRESHOLD) and changed files ($UPDATE_COUNT) / ($UP_THRESHOLD)"
     fi
     SUBJECT="[WARNING] $MSG $EMAIL_SUBJECT_PREFIX"
   elif [ -z "${JOBS_DONE##*"SYNC"*}" ] && ! grep -qw "SYNC JOB -" "$TMP_OUTPUT"; then
@@ -483,11 +482,11 @@ function output_to_file_screen(){
 
 # Sends important messages to syslog
 function mklog() {
-     [[ "$*" =~ ^([A-Za-z]*):\ (.*) ]] &&
-     {
-      PRIORITY=${BASH_REMATCH[1]} # INFO, DEBUG, WARN
-      LOGMESSAGE=${BASH_REMATCH[2]} # the Log-Message
-     }
+  [[ "$*" =~ ^([A-Za-z]*):\ (.*) ]] &&
+  {
+    PRIORITY=${BASH_REMATCH[1]} # INFO, DEBUG, WARN
+    LOGMESSAGE=${BASH_REMATCH[2]} # the Log-Message
+  }
   echo "$(date '+[%Y-%m-%d %H:%M:%S]') $(basename "$0"): $PRIORITY: '$LOGMESSAGE'" >> "$SNAPRAID_LOG"
 }
 
