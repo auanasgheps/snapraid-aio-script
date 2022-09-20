91699bbccfe0f1feae7933d91b151fd1add16225
tehniemer <jrniemand@gmail.com>
2022-09-20 06:18:56 -0500

    Bump config file version

--- snapraid-aio-script.sh
+++ snapraid-aio-script.sh
@@ -91,7 +91,7 @@ function main(){
 	mklog_noconfig "WARN: Script configuration file not found! The script cannot be run! Please check and try again!"
 	exit 1;
   # check if the config file has the correct version
-  elif [ "$CONFIG_VERSION" != 3.2 ]; then
+  elif [ "$CONFIG_VERSION" != 3.3 ]; then
     echo "Please update your config file to the latest version. The current file is not compatible with this script!"
     mklog "WARN: Please update your config file to the latest version. The current file is not compatible with this script!"
     if [ "$EMAIL_ADDRESS" ]; then
