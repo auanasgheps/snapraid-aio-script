72cd956d8a544622647fc811ef5157bb6a1a55e9
tehniemer <jrniemand@gmail.com>
2022-10-09 16:06:49 -0500

    increment dev value

--- snapraid-aio-script.sh
+++ snapraid-aio-script.sh
@@ -8,7 +8,7 @@
 ######################
 #   CONFIG VARIABLES #
 ######################
-SNAPSCRIPTVERSION=3.3 #-DEV1
+SNAPSCRIPTVERSION=3.3 #-DEV2
 
 # Read SnapRAID version
 SNAPRAIDVERSION="$(snapraid -V | sed -e 's/snapraid v\(.*\)by.*/\1/')"
