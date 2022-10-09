2d1f0aa5afeae33ca4deb1c6819f0c472d4556ce
tehniemer <jrniemand@gmail.com>
2022-10-09 16:13:08 -0500

    Update snapraid-aio-script.sh

--- snapraid-aio-script.sh
+++ snapraid-aio-script.sh
@@ -8,7 +8,7 @@
 ######################
 #   CONFIG VARIABLES #
 ######################
-SNAPSCRIPTVERSION=3.2 #-DEV4
+SNAPSCRIPTVERSION=3.2 #-DEV5
 
 # Read SnapRAID version
 SNAPRAIDVERSION="$(snapraid -V | sed -e 's/snapraid v\(.*\)by.*/\1/')"
