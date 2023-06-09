c3479f74e5e79e410b07a4b61f7421c2d458aec6
tehniemer <jrniemand@gmail.com>
2023-06-08 20:12:50 -0500

    Update dev version

--- script-config.sh
+++ script-config.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-CONFIG_VERSION=3.3 #-DEV1
+CONFIG_VERSION=3.3 #-DEV2
 ######################
 #   USER VARIABLES   #
 ######################
--- snapraid-aio-script.sh
+++ snapraid-aio-script.sh
@@ -8,7 +8,7 @@
 ########################
 #   CONFIG VARIABLES   #
 ########################
-SNAPSCRIPTVERSION=3.3 #-DEV2
+SNAPSCRIPTVERSION=3.3 #-DEV3
 
 # Read SnapRAID version
 SNAPRAIDVERSION="$(snapraid -V | sed -e 's/snapraid v\(.*\)by.*/\1/')"
